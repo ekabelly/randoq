d1bb16092c4e8c8a20d90c361f3c881687c9eac2
Eedoh <ekabelly@gmail.com>
2020-11-23 21:01:49 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,86 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" dir="rtl">
+    <div class="questions" v-if="!end">
+      <div class="topic">topic: {{ selectedQ.topic }}</div>
+      <div class="question">
+        {{ selectedQ.he }}
+      </div>
+      <div class="reroll-container">
+        <button class="reroll" @click="assignRandomQ">
+          reroll
+        </button>
+      </div>
     </div>
-    <router-view />
+    <div class="end" v-if="end">
+      נגמרו השאלות!
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      end: false,
+      selectedQ: {},
+      questions: null
+    };
+  },
+  async mounted() {
+    this.questions = await this.$store.dispatch("questions");
+    this.assignRandomQ();
+  },
+  methods: {
+    assignRandomQ() {
+      if (this.questions.length > 0) {
+        const randomQIndex = Math.floor(Math.random() * this.questions.length);
+        this.selectedQ = this.questions.splice(randomQIndex, 1)[0];
+      } else {
+        this.end = true;
+      }
     }
   }
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.questions,
+.end {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.topic,
+.question,
+.end {
+  text-align: center;
+}
+
+.topic {
+  font-size: 20px;
+}
+.question,
+.end {
+  font-size: 30px;
+}
+
+.reroll-container {
+  margin-top: 20vh;
+  display: flex;
+  justify-content: center;
+}
+
+.reroll {
+  font-size: 30px;
+  width: 60vw;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,30 @@
+import Axios from "axios";
 import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    questions: null
+  },
+  mutations: {
+    questions(state, questions) {
+      state.questions = questions;
+    }
+  },
+  actions: {
+    async questions(context) {
+      const res = await Axios.get(
+        "https://randoq-app.firebaseio.com/questions.json"
+      );
+      const questions = res.data.filter(q => !!q);
+      context.commit("questions", questions);
+      return questions;
+    }
+  },
+  getters: {
+    questions: state => state.questions
+  },
   modules: {}
 });
